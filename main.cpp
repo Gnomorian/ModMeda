4e5431d5f6d0da5de902184e95dfc992f0f91a7e
William Cameron <william-cameron1994@live.co.uk>
2022-03-17 23:23:29 +1300

    added a help string

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
-#include <string_view>
-#include <vector>
-#include <algorithm>
-#include <iterator>
 #include "Commandline.h"
-
-
-auto getCommandline(auto argc, auto args)
-{
-	std::vector<std::wstring_view> cmdline;
-	std::generate_n(std::back_inserter(cmdline), argc, [args, i = 0]()mutable {return args[i]; });
-	return cmdline;
-}
+#include "strings.h"
 
 int wmain(int argc, wchar_t* args[])
 {
 	Commandline cmd{ argc, args };
+	std::wcout << HelpText << std::endl;
 }
---
+++ strings.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string_view>
+
+constexpr std::wstring_view HelpText{
+	LR"(commandline arguments:
+	path=<path>
+		optional argument that gives a root directory. if this is not supplied then the root directory is the current working directory.
+	recursive
+		option argument that tells the application to modify all files that fit the filter in all directories recursivly
+	filename=<filename>
+		the filename to do the action on. cannot be a commandline argument if filter is.
+	filter=<filterRegex>
+		cannot be a commandline argument if filename is.
+	action=<list|modify>
+		list=list the metadata keys and values for the given <filename> or <filter>
+		modify=modify the metadata keys and values for the given <filename> or <filter>
+	key=<keyname>
+		the key string to modify for the given <filename> or files that match <filter>. keys can be listed by action=list.
+	value=<valuestring>
+		what to change the value of the given key to.
+	type=<>
+		when the key doesnt exist in the file, we need to know what the type of the new metadata key is going to be.
+Examples:
+	ModMeta.exe filename="example.mp3" path="c:\music" action=list
+		list the metadata keys and values for the file c:\music\example.mp3
+	ModMeta.exe filter=".*\.mp3" action=modify key=author value="Myself"
+		for all files in the working directory that match the regex ".*\.mp3", modify the author metadata to say "myself"
+)"
+};
