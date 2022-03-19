f5d723e075a467d2d5c398607f1c9e69cb542144
William Cameron <william-cameron1994@live.co.uk>
2022-03-19 15:22:46 +1300

    changed windows media player sdk metadata to WinRT

--- source/CommandListFileMetadata.cpp
+++ source/CommandListFileMetadata.cpp
@@ -1,9 +1,15 @@
 #include "CommandListFileMetadata.h"
 #include <variant>
 #include <Windows.h>
-#include <Wmsdk.h>
-#include <mfidl.h>
-#pragma comment(lib, "Wmvcore.lib")
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Storage.FileProperties.h>
+#include <iomanip>
+#include <chrono>
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Storage;
 
 CommandListFileMetadata::CommandListFileMetadata(std::wostream* output, const Commandline& commandline)
 	: Command{output}
@@ -12,107 +18,90 @@ CommandListFileMetadata::CommandListFileMetadata(std::wostream* output, const Co
 
 void CommandListFileMetadata::execute()
 {
-	IWMMetadataEditor* editor{ nullptr };
-	auto result{ WMCreateEditor(&editor) };
-	if (result != S_OK)
-	{
-		*output << "WMCreateEditor error, " << result;
-		return;
-	}
 	const auto filename{ normaliseFilename() };
-	result = editor->Open(filename.c_str());
-	if (result != S_OK)
-	{
-		*output << "IWMMetadataEditor::Open error, " << result << std::endl;
-		return;
-	}
-	*output << "Listing metadata for " << filename << std::endl;
+    init_apartment();
+    auto file{ StorageFile::GetFileFromPathAsync(hstring{ filename.c_str() }).get()};
+    if (file)
+    {
 
-	IWMHeaderInfo3* headerinfo{ nullptr };
-	result = editor->QueryInterface(&headerinfo);
-	if (result != S_OK)
-	{
-		*output << "IWMMetadataEditor::QueryInterface<IWMHeaderInfo3**> error, " << result << std::endl;
-		return;
-	}
-	WORD numAttributes{};
-	result = headerinfo->GetAttributeCount(0xFFFF, &numAttributes);
-	if (result != S_OK)
-	{
-		*output << "IWMHeaderInfo3::GetAttributeCount error, " << result << std::endl;
-		return;
-	}
-	std::vector<wchar_t> nameBuffer;
-	std::vector<BYTE> dataBuffer;
-	using Metadata = std::variant<std::monostate, std::wstring, DWORD, WORD, QWORD, bool, std::vector<char>>;
-	std::vector<std::pair<std::wstring, Metadata>> metadataForFile;
-	for (WORD i{ 0 }; i < numAttributes; i++)
-	{
-		WORD streamNum{ 0xFFFF };
-		WORD nameLen{};
-		WMT_ATTR_DATATYPE dataType{};
-		WORD dataLen{};
-		result = headerinfo->GetAttributeByIndex(i, &streamNum, nullptr, &nameLen, &dataType, nullptr, &dataLen);
-		if (result != S_OK)
-		{
-			*output << "IWMHeaderInfo3::GetAttributeByIndex error, " << result << std::endl;
-			return;
-		}
-		nameBuffer.resize(nameLen);
-		dataBuffer.resize(dataLen);
-		result = headerinfo->GetAttributeByIndex(i, &streamNum, nameBuffer.data(), &nameLen, &dataType, dataBuffer.data(), &dataLen);
-		if (result != S_OK)
-		{
-			*output << "IWMHeaderInfo3::GetAttributeByIndex error, " << result << std::endl;
-			return;
-		}
-		std::wstring name{ nameBuffer.data(), nameLen };
-		switch (dataType)
-		{
-		case WMT_TYPE_STRING:
-			metadataForFile.emplace_back(std::move(name), Metadata{ std::in_place_index<1>, std::wstring(reinterpret_cast<wchar_t*>(dataBuffer.data()), static_cast<size_t>(dataLen)) });
-			break;
-		case WMT_TYPE_DWORD:
-			metadataForFile.emplace_back(std::move(name), Metadata{std::in_place_index<2>, static_cast<DWORD>(*dataBuffer.data()) });
-			break;
-		case WMT_TYPE_WORD:
-			metadataForFile.emplace_back(std::move(name), Metadata{ std::in_place_index<3>, static_cast<WORD>(*dataBuffer.data()) });
-			break;
-		case WMT_TYPE_QWORD:
-			metadataForFile.emplace_back(std::move(name), Metadata{ std::in_place_index<4>, static_cast<QWORD>(*dataBuffer.data()) });
-			break;
-		case WMT_TYPE_BOOL:
-			metadataForFile.emplace_back(std::move(name), Metadata{ std::in_place_index<5>, static_cast<bool>(*dataBuffer.data()) });
-			break;
-		case WMT_TYPE_BINARY:
-			metadataForFile.emplace_back(std::move(name), Metadata{ std::in_place_index<6>, std::vector<char>{dataBuffer.begin(), dataBuffer.begin() + dataLen}});
-			break;
-		default:
-			metadataForFile.emplace_back(std::move(name), Metadata{std::in_place_index<0>});
-		}
-	}
-	*output << L"There where " << numAttributes << L" metadata attributes in the file.\n";
-	for (int i{ 0 }; const auto & metadata : metadataForFile)
-	{
-		*output << ++i << L") " << metadata.first << L" = ";
-		std::visit([&](const auto& variant) 
-			{
-				using Type = std::decay<decltype(variant)>::type;
-				if constexpr (std::is_integral<Type>::value || std::is_same<std::wstring, Type>{})
-				{
-					*output << variant << std::endl;
-				}
-				else
-				{
-					*output << "not a value im handling yet" << std::endl;
-				}
-			}, 
-			metadata.second
-		);
-	}
+        //auto basicProperties{ file.Properties().RetrievePropertiesAsync({}).get() };
+        auto basicProperties{ file.GetBasicPropertiesAsync().get().RetrievePropertiesAsync({}).get() };
+        for (int i{}; const auto & p : basicProperties)
+        {
+            const auto key{ p.Key().c_str() };
+            std::wcout << ++i << L") " << key;
+            if (p.Value())
+            {
+                std::wcout << " = ";
+                auto value{ p.Value().as<IPropertyValue>() };
+                const auto propertyType{ value.Type() };
+                switch (propertyType)
+                {
+                case PropertyType::DateTime:
+                {
+                    const auto timepoint{ value.GetDateTime() };
+                    using Type = decltype(timepoint);
+                    const auto t_c{ Type::clock::to_time_t(timepoint) };
+                    tm time{};
+                    const auto result{ localtime_s(&time, &t_c) };
+                    std::wcout << std::put_time(&time, L"%F %T.\n");
+                    break;
+                }
+                case PropertyType::String:
+                {
+                    const auto string{ value.GetString() };
+                    std::wcout << string.c_str();
+                    break;
+                }
+                case PropertyType::Boolean:
+                {
+                    const auto boolean{ value.GetBoolean() };
+                    std::wcout << std::boolalpha << boolean;
+                    break;
+                }
+                case PropertyType::Double:
+                    std::wcout << value.GetDouble();
+                    break;
+                case PropertyType::Char16:
+                    std::wcout << static_cast<wchar_t>(value.GetChar16());
+                    break;
+                case PropertyType::Int16:
+                    std::wcout << static_cast<int16_t>(value.GetInt16());
+                    break;
+                case PropertyType::UInt32:
+                    std::wcout << static_cast<uint32_t>(value.GetUInt32());
+                    break;
+                case PropertyType::Int32:
+                    std::wcout << static_cast<int32_t>(value.GetInt32());
+                    break;
+                case PropertyType::Int64:
+                    std::wcout << static_cast<int64_t>(value.GetInt64());
+                    break;
+                case PropertyType::UInt64:
+                    std::wcout << static_cast<uint64_t>(value.GetUInt64());
+                    break;
+                case PropertyType::StringArray:
+                {
+                    com_array<hstring> strArray;
+                    value.GetStringArray(strArray);
+                    for (bool first{ true }; const auto & str : strArray)
+                    {
+                        if (first)
+                            first = false;
+                        else
+                            std::wcout << L';';
+                        std::wcout << L'"' << str.c_str() << L'"';
 
-	headerinfo->Release();
-	editor->Release();
+                    }
+                    break;
+                }
+                default:
+                    std::wcout << L"? type = " << static_cast<int>(propertyType);
+                }
+            }
+            std::wcout << std::endl;
+        }
+    }
 }
 
 std::filesystem::path CommandListFileMetadata::normaliseFilename() const
