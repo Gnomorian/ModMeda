36d5681d03ad4ee8c7189a5fb21f377fb6396f94
William Cameron <william-cameron1994@live.co.uk>
2022-04-10 17:34:12 +1200

    added structure for list all proeprties

--- include/CommandListFolderMetadata.h
+++ include/CommandListFolderMetadata.h
@@ -5,5 +5,13 @@ class CommandListFolderMetadata : public Command
 public:
 	CommandListFolderMetadata(std::wostream* output, const Commandline& commandline);
 	void execute() override;
+	void onEmptyPropertyGroup() override;
+	void onAllPropertyGroup() override;
+	void onBasicPropertyGroup() override;
+	void onDocumentPropertyGroup() override;
+	void onMusicPropertyGroup() override;
+	void onVideoPropertyGroup() override;
+	void onAttriburePropertyGroup() override;
+	void onImagePropertyGroup() override;
 };
 
--- source/CommandListFolderMetadata.cpp
+++ source/CommandListFolderMetadata.cpp
@@ -2,9 +2,47 @@
 
 CommandListFolderMetadata::CommandListFolderMetadata(std::wostream* output, const Commandline& commandline)
 	: Command{ output, commandline }
-{}
+{
+	if (!commandline.hasKey(L"filter") || commandline.getAtKey(L"action").second != L"list")
+	{
+		throw std::invalid_argument{ "" };
+	}
+}
 
 void CommandListFolderMetadata::execute()
 {
+	handlePropertyGroupCase();
+}
 
+void CommandListFolderMetadata::onEmptyPropertyGroup()
+{
+
+}
+
+void CommandListFolderMetadata::onAllPropertyGroup()
+{
+}
+
+void CommandListFolderMetadata::onBasicPropertyGroup()
+{
+}
+
+void CommandListFolderMetadata::onDocumentPropertyGroup()
+{
+}
+
+void CommandListFolderMetadata::onMusicPropertyGroup()
+{
+}
+
+void CommandListFolderMetadata::onVideoPropertyGroup()
+{
+}
+
+void CommandListFolderMetadata::onAttriburePropertyGroup()
+{
+}
+
+void CommandListFolderMetadata::onImagePropertyGroup()
+{
 }
